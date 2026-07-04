557b42001e7c081fa2dbbba1e152982ee348e4e9
Mohitrajhu Lingan Kumaraian <mohit.kumaraian@gmail.com>
2026-07-04 14:22:30 -0500

    Add: sql-team-scores-in-football-tournament - submission-1

---
+++ SQL/sql-team-scores-in-football-tournament/submission-1.sql
@@ -0,0 +1,25 @@
+with score_calculation as (
+select host_team as team, sum(1) as points
+from matches
+where host_goals = guest_goals
+group by team
+union all
+select guest_team as team, sum(1) as points
+from matches
+where host_goals = guest_goals
+group by team
+union all
+select case
+when host_goals > guest_goals then host_team
+else guest_team
+end as team, sum(3) as points
+from matches
+where host_goals <> guest_goals
+group by team
+)
+select t.team_id, t.team_name, coalesce(sum(a.points),0) as num_points 
+from teams t
+left join score_calculation a
+on a.team = t.team_id
+group by t.team_id, t.team_name
+order by num_points desc, t.team_id;
